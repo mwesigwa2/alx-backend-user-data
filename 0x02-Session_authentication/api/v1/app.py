c364674e7082875e158ba23d9c3f1429792f05b9
Timothy Kibirige <tmwesigwa2@gmail.com>
2024-02-14 14:38:01 +0000

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,6 +30,7 @@ def before_request():
     if auth is None:
         pass
     else:
+        request.current_user = auth.current_user(request)
         excluded_list = ['/api/v1/status/',
                          '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
